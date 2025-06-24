8f652b7fbe9e161e0b287435eea0307afa8774f6
Caldeiro, Luis M. <caldeiro@protonmail.com>
2025-06-24 21:19:36 +0200

    -nojekyll

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
- output: 'export',
- basePath: '/2budget',
- assetPrefix: '/2budget/',
- eslint: {
-   ignoreDuringBuilds: true,
- },
- typescript: {
-   ignoreBuildErrors: true,
- },
- images: {
-   unoptimized: true,
- }
+  output: 'export',
+  basePath: '/2budget',
+  assetPrefix: '/2budget/',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
